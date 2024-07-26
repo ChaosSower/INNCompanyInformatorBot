abccff6058b0413ce7246f1892bdd7848ed5f9fb
ChaosSower <evtykhin_work@yahoo.com>
2024-07-26 14:13:17 +0300

    Налаживание метода считывания ключей со значениями из файла App.config

--- INNCompanyInformatorBot/Program.cs
+++ INNCompanyInformatorBot/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+
+System.Collections.Specialized.NameValueCollection AppSettings = ConfigurationManager.AppSettings;
+
+foreach (string? Key in AppSettings.AllKeys)
+{
+    Console.WriteLine("Key: {0} Value: {1}", Key, AppSettings[Key]);
+}
